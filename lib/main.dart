671bb5941f27b16ad4f3a73ac4d3e1c3fd1dca96
Justin Canzonetta <canzonej@oregonstate.edu>
2023-07-09 00:02:09 -0700

    Initialize Firebase plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'app.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
