2db45e81fab3ffb0c1696d0232ecafa769246d0a
Justin Canzonetta <canzonej@oregonstate.edu>
2023-07-09 21:29:54 -0700

    Add observation and water_temperature form field widgets

--- lib/screens/create_report_screen.dart
+++ lib/screens/create_report_screen.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
-class CreateReportScreen extends StatelessWidget {
+import '../models/user_report.dart';
+import '../widgets/forms/observation_form_field.dart';
+import '../widgets/forms/water_temperature_form_field.dart';
+
+class CreateReportScreen extends StatefulWidget {
   static const String routeName = 'CreateReportScreen';
 
   const CreateReportScreen({super.key});
 
   @override
+  State<CreateReportScreen> createState() => _CreateReportScreenState();
+}
+
+class _CreateReportScreenState extends State<CreateReportScreen> {
+  final formKey = GlobalKey<FormState>();
+  final userReport = UserReport();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Report an Observation')),
-      body: const Placeholder(),
+      body: Form(
+        key: formKey,
+        child: SingleChildScrollView(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            ObservationFormField(userReport: userReport),
+            WaterTemperatureFormField(userReport: userReport),
+          ]),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/forms/water_temperature_form_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:ocean_change/models/user_report.dart';
+
+class WaterTemperatureFormField extends StatelessWidget {
+  const WaterTemperatureFormField({
+    super.key,
+    required this.userReport,
+  });
+
+  final UserReport userReport;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextFormField(
+          decoration:
+              const InputDecoration(labelText: 'Water Temperature (°F)'),
+          keyboardType: const TextInputType.numberWithOptions(signed: false),
+          maxLength: 5,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,1}'))
+          ],
+          onSaved: (newValue) {
+            if (newValue?.isEmpty ?? true) {
+              userReport.waterTemp = null;
+            } else {
+              userReport.waterTemp = int.parse(newValue!);
+            }
+          }),
+    );
+  }
+}
