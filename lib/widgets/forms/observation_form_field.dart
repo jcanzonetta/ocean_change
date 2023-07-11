c31118ca21a3ddf5c8846a326144618c56a8cabc
Justin Canzonetta <canzonej@oregonstate.edu>
2023-07-10 23:27:22 -0700

    Add observation label in form

--- lib/widgets/forms/observation_form_field.dart
+++ lib/widgets/forms/observation_form_field.dart
@@ -27,21 +27,33 @@ class _ObservationFormFieldState extends State<ObservationFormField> {
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(8.0),
-        child: DropdownButton<String>(
-          value: widget.userReport.observation,
-          icon: const Icon(Icons.arrow_downward),
-          onChanged: (String? value) {
-            setState(() {
-              widget.userReport.observation = value!;
-            });
-          },
-          items: ObservationFormField.observationList
-              .map<DropdownMenuItem<String>>((String value) {
-            return DropdownMenuItem<String>(
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('What did you see?'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButton<String>(
+                value: widget.userReport.observation,
+                icon: const Icon(Icons.arrow_downward),
+                onChanged: (String? value) {
+                  setState(() {
+                    widget.userReport.observation = value!;
+                  });
+                },
+                items: ObservationFormField.observationList
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
         ));
   }
 }
