77d0a2839e762e12187004e5334f57255ffa43ed
Justin Canzonetta <canzonej@oregonstate.edu>
2023-07-16 21:58:43 -0700

    Add image upload feature

--- lib/widgets/forms/submit_form_button.dart
+++ lib/widgets/forms/submit_form_button.dart
@@ -1,19 +1,38 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:ocean_change/models/user_report.dart';
 
 class SubmitFormButton extends StatefulWidget {
   final GlobalKey<FormState> formKey;
   final UserReport userReport;
+  final File? image;
 
   const SubmitFormButton(
-      {super.key, required this.formKey, required this.userReport});
+      {super.key, required this.formKey, required this.userReport, this.image});
 
   @override
   State<SubmitFormButton> createState() => _SubmitFormButton();
 }
 
 class _SubmitFormButton extends State<SubmitFormButton> {
+  Future<String> uploadImage(File image) async {
+    String fileName =
+        '${widget.userReport.observation!}${DateFormat('MMddyyyy').format(widget.userReport.date!)}.jpg';
+
+    // Upload image to Cloud Storage
+    Reference storageReference = FirebaseStorage.instance.ref().child(fileName);
+
+    UploadTask uploadTask = storageReference.putFile(widget.image!);
+    await uploadTask;
+
+    // Receive url of photo
+    return storageReference.getDownloadURL();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -23,12 +42,18 @@ class _SubmitFormButton extends State<SubmitFormButton> {
           if (widget.formKey.currentState?.validate() ?? true) {
             widget.formKey.currentState!.save();
 
+            String? imageUrl;
+            if (widget.image != null) {
+              imageUrl = await uploadImage(widget.image!);
+            }
+
             FirebaseFirestore.instance.collection('reports').add({
               'observation': widget.userReport.observation,
               'species': widget.userReport.species,
               'observation_number': widget.userReport.observationNumber,
               'water_temp': widget.userReport.waterTemp,
-              'date': widget.userReport.date
+              'date': widget.userReport.date,
+              'photo_url': imageUrl
             });
 
             Navigator.of(context).pop();
