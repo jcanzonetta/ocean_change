45fc56d6bf97dcd6c6fb0cc8ad8b3ec0925b9e43
jacisback <cohrsja@oregonstate.edu>
2023-07-24 23:29:34 +0100

    First round of UI formatting for view report screen and marker popups.

---
+++ lib/styles.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+class Styles {
+  static const viewScreenMediumLabels =
+      TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold);
+
+  static const viewScreenMediumData =
+      TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.normal);
+
+  static const viewScreenSmallLabels =
+      TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold);
+
+  static const viewScreenSmallData =
+      TextStyle(fontSize: 16, color: Colors.black, fontWeight: FontWeight.normal);
+
+  static const viewScreenObservationLabels =
+      TextStyle(fontSize: 24, color: Colors.black, fontWeight: FontWeight.bold);
+
+  static const viewScreenObservationData =
+      TextStyle(fontSize: 24, color: Colors.black, fontWeight: FontWeight.normal);
+}
