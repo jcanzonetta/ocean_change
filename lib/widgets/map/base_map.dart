251189f140549c7eda912c1ba24545ee63971317
Justin Canzonetta <canzonej@oregonstate.edu>
2023-07-25 21:40:35 -0700

    Merge main

--- lib/widgets/map/base_map.dart
+++ lib/widgets/map/base_map.dart
@@ -1,17 +1,15 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:ocean_change/screens/map_screen.dart';
 import 'package:ocean_change/widgets/map/popup.dart';
 import 'package:ocean_change/widgets/map/report_popup.dart';
 import 'package:ocean_change/widgets/map/report_marker.dart';
-import 'package:ocean_change/models/user_report.dart';
-import 'package:ocean_change/widgets/map/report_marker_icon.dart';
+
 
 class BaseMap extends StatefulWidget {
-  const BaseMap({super.key});
+  final List<ReportMarker> reportMarkers;
+  BaseMap({super.key, required this.reportMarkers});
 
   @override
   State<BaseMap> createState() => _BaseMapState();
@@ -19,7 +17,6 @@ class BaseMap extends StatefulWidget {
 
 class _BaseMapState extends State<BaseMap> {
   final mapController = MapController();
-  List<ReportMarker> reportMarkers = [];
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +39,7 @@ class _BaseMapState extends State<BaseMap> {
         ),
         PopupMarkerLayerWidget(
           options: PopupMarkerLayerOptions(
-              markers: generateMarkers(context),
+              markers: widget.reportMarkers,
               popupBuilder: (BuildContext context, Marker marker) {
                 if (marker is ReportMarker) {
                   return ReportPopUp(marker, marker.userReport);
@@ -53,28 +50,5 @@ class _BaseMapState extends State<BaseMap> {
         )
       ],
     );
-  }
-
-  List<ReportMarker> generateMarkers(BuildContext context) {
-    // reaches up to map screen to determine if it should generate markers
-    MapScreenState? mapScreenState =
-        context.findAncestorStateOfType<MapScreenState>();
-    if (mapScreenState?.markersReady == false) {
-      reportMarkers.clear();
-      FirebaseFirestore.instance.collection('reports').get().then(
-        (event) {
-          for (var doc in event.docs) {
-            final report = UserReport.fromFirestore(doc.data());
-            reportMarkers.add(ReportMarker(
-                userReport: report,
-                builder: (context) =>
-                    ReportMarkerIcon(observation: report.observation)));
-          }
-          mapScreenState?.toggleMarkersReady();
-          setState(() {});
-        },
-      );
-    }
-    return reportMarkers;
   }
 }
