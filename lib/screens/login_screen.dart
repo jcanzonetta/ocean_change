a5f4e04af40032c5a504561f525032224b022283
jacisback <cohrsja@oregonstate.edu>
2023-07-27 23:28:14 +0100

    Added initial user authentication.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ocean_change/screens/create_account_screen.dart';
+import '../models/user_data.dart';
+import '../widgets/login/login_error_alert.dart';
+
 
 class LoginScreen extends StatefulWidget {
   static const String routeName = 'LoginScreen';
@@ -11,50 +15,73 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final formKey = GlobalKey<FormState>();
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
+  UserData userData = UserData();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            const Padding(
-                padding: EdgeInsets.fromLTRB(0, 24, 0, 6),
-                child: Text("First time user?")),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 48),
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, CreateAccountScreen.routeName);
-                  }, 
-                  child: const Text("Create Account")),
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: "Email"),
-              // TO DO: Add validator
-            ),
-            const TextField(
-              decoration: InputDecoration(labelText: "Password"),
-              obscureText: true,
-              // TO DO: Add validator
-            ),
-            ElevatedButton(onPressed: _signIn, child: const Text("Log In"))
-          ],
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ocean Change')),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 24, 0, 6),
+                  child: Text("First time user?")),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 48),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(
+                          context, CreateAccountScreen.routeName);
+                    },
+                    child: const Text("Create Account")),
+              ),
+              TextFormField(
+                  decoration: const InputDecoration(labelText: "Email"),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter a username";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    userData.email = value!;
+                  }),
+              TextFormField(
+                  decoration: const InputDecoration(labelText: "Password"),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter a password";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onSaved: (value) {
+                    userData.password = value!;
+                  }),
+              ElevatedButton(onPressed: _signIn, child: const Text("Log In"))
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future _signIn() async {
-    formKey.currentState!.save();
-    await FirebaseAuth.instance
-        .signInWithEmailAndPassword(email: emailController.text, password: passwordController.text);
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+            email: userData.email!, password: userData.password!);
+      } on FirebaseAuthException catch (e) {
+        showLoginError(context, e.message!);
+      }
+    }
   }
-
-
-  
 }
+
